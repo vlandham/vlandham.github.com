f329da1d0b454ac736da94290b5d743c7809e696
Jim Vallandingham <vlandham@gmail.com>
2017-05-29 14:11:29 -0700

    add underlines on all the things.

--- js/pages/vis.js
+++ js/pages/vis.js
@@ -1,31 +1,32 @@
 
-function slideDown (element, duration, finalheight, callback) {
-    var s = element.style;
-    s.height = '0px';
-
-    var y = 0;
-    var framerate = 10;
-    var one_second = 1000;
-    var interval = one_second*duration/framerate;
-    var totalframes = one_second*duration/interval;
-    var heightincrement = finalheight/totalframes;
-    var tween = function () {
-        y += heightincrement;
-        s.height = y+'px';
-        if (y<finalheight) {
-            setTimeout(tween,interval);
-        }
-    }
-    tween();
-}
+// function slideDown (element, duration, finalheight, callback) {
+//     var s = element.style;
+//     s.height = '0px';
+//
+//     var y = 0;
+//     var framerate = 10;
+//     var one_second = 1000;
+//     var interval = one_second*duration/framerate;
+//     var totalframes = one_second*duration/interval;
+//     var heightincrement = finalheight/totalframes;
+//     var tween = function () {
+//         y += heightincrement;
+//         s.height = y+'px';
+//         if (y<finalheight) {
+//             setTimeout(tween,interval);
+//         }
+//     }
+//     tween();
+// }
 
 $(document).ready(function(){
-  $(".banner-toggle").on("click", function(e){
-    // $(this).slideToggle();
+
+  $(".banner").on("mouseover", function(e) {
     var el = $(this);
-    var id = el.parents('.section').first().attr('id');
-    el.siblings('div.details').slideToggle();
-    ga('send', 'event', id, 'toggle');
-    e.preventDefault();
+    el.parents('.details').first().children('h2').css('text-decoration', 'underline');
+  });
+  $(".banner").on("mouseout", function(e) {
+    var el = $(this);
+    el.parents('.details').first().children('h2').css('text-decoration', 'none');
   });
 });
